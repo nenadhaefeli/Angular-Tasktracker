d600f555ff130ec6c60bbf7a282a4906a38f86ff
nenad.haefeli <nenad.haefeli@gmail.com>
2021-11-26 15:00:37 +0100

    added tasks with fake DB, implemented task delete and reminder toggle

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 import {Task} from '../../Task'
-import {TASKS} from '../../mock-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -8,11 +8,20 @@ import {TASKS} from '../../mock-tasks';
   styleUrls: ['./tasks.component.sass']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter(t => t.id !== task.id)));
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
 }
